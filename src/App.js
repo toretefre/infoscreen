58ce6bb79788edf450b5eb42926d79c9ce8980ed
Tore Stensaker Tefre <tore@tefre.it>
2019-09-05 22:35:08 +0200

    introduced weathercard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Card from './components/Card';
 import './App.css';
+import WeatherCard from './components/WeatherCard';
 
 export const App = () => {
   return (
     <article class="article">
       <Card title="Buss" />
       <Card title="Bysykkel" />
-      <Card title="Vær" />
+      <WeatherCard />
       <Card title="Har mannen falt?" />
     </article>
   );
---
+++ src/components/WeatherCard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const WeatherCard = () => {
+  return (
+    <section className="card">
+      <h1>Vær</h1>
+    </section>
+  );
+};
+
+export default WeatherCard;
