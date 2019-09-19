701cd595c67a62a31a94571b88e66b4197b8cfb1
Tore Stensaker Tefre <tore@tefre.it>
2019-09-19 23:27:00 +0200

    css reset margin + paddings

--- src/components/BusCard/index.js
+++ src/components/BusCard/index.js
@@ -9,15 +9,15 @@ export const BusCard = () => {
     <section className="card">
       <section>
         <h2>3 Lohove</h2>
-        <h1>3</h1>
+        <h1 className="bigtext">2</h1>
         <p>min</p>
         <p>Deretter 07:41, 07:48 og 07:55</p>
       </section>
       <section>
         <h2>22 Vestlia</h2>
-        <p>3 min</p>
-        <p>10 min</p>
-        <p>13:42</p>
+        <h1 className="bigtext">3</h1>
+        <p>min</p>
+        <p>Deretter 07:41, 07:48 og 07:55</p>
       </section>
     </section>
   );
