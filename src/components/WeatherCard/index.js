3c11d1d9783be5e7c60a2459d9c390b286be1083
Tore Stensaker Tefre <tore@tefre.it>
2020-04-11 00:00:04 +0200

    easier variable naming + norwegian wind directions

--- src/components/WeatherCard/index.js
+++ src/components/WeatherCard/index.js
@@ -3,26 +3,35 @@ import moment from 'moment';
 import * as convert from 'xml-js';
 import * as V from 'victory';
 
+const directions = {
+  SW: "sør-vest",
+  SE: "sør-aust",
+  NW: "nord-vest",
+  NE: "nord-aust",
+  W: "vest",
+  N: "nord",
+  S: "sør",
+  E: "aust",
+}
+
 export const WeatherCard = () => {
   const [weather, setWeather] = useState();
   const [precipitation, setPrecipitation] = useState();
 
   useEffect(() => {
-    const fetchWeatherData = async () => {
+    const fetchPrecipitation = async () => {
       const response = await fetch(
         'https://cors-anywhere.herokuapp.com/https://www.yr.no/sted/Norge/Tr%C3%B8ndelag/Trondheim/Trondheim/varsel_nu.xml'
       );
       const xmlfile = await response.text()
       const jsfile = convert.xml2js(xmlfile);
-      const currentWeather = jsfile.elements[0].elements;
+      const currentPrecipitation = jsfile.elements[0].elements;
 
-      console.log(currentWeather)
-
-      if (!currentWeather.find(element => element.name === "forecast").elements) {
+      if (!currentPrecipitation.find(element => element.name === "forecast").elements) {
         return;
       }
 
-      const precipitationData = currentWeather
+      const precipitationData = currentPrecipitation
         .find(element => element.name === "forecast")
         .elements;
 
@@ -39,7 +48,7 @@ export const WeatherCard = () => {
         });
       console.log(precipitationChartData);
 
-      const lastUpdatedTime = currentWeather
+      const lastUpdatedTime = currentPrecipitation
         .find(element => element.name === "meta").elements
         .find(element => element.name === "lastupdate").elements[0].text;
 
@@ -49,34 +58,32 @@ export const WeatherCard = () => {
       });
     };
 
-    const fetchTemperatureData = async () => {
+    const fetchForecast = async () => {
       const response = await fetch(
         'https://api.met.no/weatherapi/locationforecast/1.9/.json?lat=63.422937&lon=10.396857&msl=10'
       )
       const temperatureData = await response.json();
-      const newestTemperatureData = temperatureData.product.time[0];
+      const forecast = temperatureData.product.time[0];
       const symbolData = temperatureData.product.time[1].location.symbol;
 
-
-      console.log(newestTemperatureData)
       setWeather({
         symbol: {
           code: symbolData.number,
           id: symbolData.id,
         },
-        temperature: newestTemperatureData.location.temperature.value,
-        cloudiness: newestTemperatureData.location.cloudiness.percent,
+        temperature: forecast.location.temperature.value,
+        cloudiness: forecast.location.cloudiness.percent,
         wind: {
-          mps: newestTemperatureData.location.windSpeed.mps,
-          name: newestTemperatureData.location.windSpeed.name,
-          direction: newestTemperatureData.location.windDirection.name,
+          mps: forecast.location.windSpeed.mps,
+          name: forecast.location.windSpeed.name,
+          direction: forecast.location.windDirection.name,
         },
-        updated: newestTemperatureData.from,
+        updated: forecast.from,
       });
     };
 
-    fetchWeatherData();
-    fetchTemperatureData();
+    fetchPrecipitation();
+    fetchForecast();
   }, []);
 
   if (!weather || !precipitation) {
@@ -91,9 +98,10 @@ export const WeatherCard = () => {
       <img src={'https://api.met.no/weatherapi/weathericon/1.1/?content_type=image%2Fpng&symbol=' + weather.symbol.code} alt={weather.symbol.id} />
       <h1 className="bigtext">{weather.temperature}&deg;</h1>
       <h2>{Math.round(weather.cloudiness)}% skydekke</h2>
-      <h2>{weather.wind.name} - {weather.wind.direction} {weather.wind.mps} m/s</h2>
+      <h2>{weather.wind.name} - {weather.wind.mps} m/s frå {directions[weather.wind.direction]}</h2>
       <h6>Vêrvarsel frå Yr, levert av NRK og Meteorologisk institutt</h6>
       <h6>Sist oppdatert {precipitation.lastUpdated.format('LT')}</h6>
+
       <V.VictoryChart>
         <V.VictoryArea
           data={precipitation.chartData}
