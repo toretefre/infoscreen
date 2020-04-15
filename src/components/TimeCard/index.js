7f928b52e898d8944f3dcd9c4a8c73630e782536
Tore Stensaker Tefre <tore@tefre.it>
2020-04-15 14:30:13 +0200

    display next public holiday

--- src/components/TimeCard/index.js
+++ src/components/TimeCard/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import 'moment-timezone';
-import holidays from './holidays.json';
+import holidayFile from './holidays.json';
 
 export const TimeCard = props => {
   const { time, geoLocation } = props;
   const [sunData, setSunData] = useState();
+  const [holidays, setHolidays] = useState();
 
   useEffect(() => {
     const fetchSunData = async () => {
@@ -16,16 +17,17 @@ export const TimeCard = props => {
       setSunData({
         sunrise: results.sunrise,
         sunset: results.sunset,
-      })
+      });
     }
 
     const findHolidays = () => {
-      const now = moment();
-      console.log(holidays)
+      const now = moment()
+      const futureHolidays = holidayFile.filter(holiday => moment(holiday.date, "DD.MM.YYYY").isAfter(now))
+      setHolidays(futureHolidays)
     }
 
-    findHolidays();
     fetchSunData();
+    findHolidays();
   }, [])
 
   if (!time) return <section id="timeCard" className="card" />;
@@ -40,6 +42,7 @@ export const TimeCard = props => {
       {sunData && <img className="sunSymbol" alt="Soloppgang og solnedgang" src={process.env.PUBLIC_URL + 'sun.png'} />}
       {sunData && <h3>{moment(sunData.sunrise).format('LT')} - {moment(sunData.sunset).format('LT')}</h3>}
       <h6 className="credits">Soldata frå sunrise-sunset.org</h6>
+      {holidays && <h4>Neste helligdag er {holidays[0].name} {moment(holidays[0].date, "DD.MM.YYYY").format('Do MMMM')}</h4>}
     </section>
   );
 };
