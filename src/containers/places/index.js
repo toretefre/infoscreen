40af6e7745b5133d14e097fe21f38e3bfe038b6b
Tore Stensaker Tefre <tore@tefre.it>
2020-04-19 20:22:41 +0200

    add firebase config

--- src/containers/places/index.js
+++ src/containers/places/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from './node_modules/react';
+import * as firebase from './node_modules/firebase'
 
 export const Places = () => {
-    return <h1>Her kan du opprette dine eigne skjermar</h1>
+    const config = {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID,
+    };
+
+    firebase.initializeApp(config);
+
+    return (
+        <article>
+            <h1>Her kan du opprette dine eigne skjermar</h1>
+        </article>
+    )
 }
 
 export default Places;
