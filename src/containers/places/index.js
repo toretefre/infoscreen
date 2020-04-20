e4ccb09e862c7fcf5397c11ee5545760d5bfb030
Tore Stensaker Tefre <tore@tefre.it>
2020-04-20 10:42:55 +0200

    prepare for secret firebase env variables

--- src/containers/places/index.js
+++ src/containers/places/index.js
@@ -10,7 +10,6 @@ export const Places = () => {
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
-        measurementId: process.env.MEASUREMENT_ID,
     };
 
     firebase.initializeApp(config);
