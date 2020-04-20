f23795e21947500ab8c775b7ca2178a3c526ca06
Tore Stensaker Tefre <tore@tefre.it>
2020-04-20 11:37:55 +0200

    fix import errors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import 'moment/locale/nn';
 import moment from 'moment';
 import { Router } from '@reach/router'
-import Home from './containers/Home'
-import Places from './containers/Places';
+import Home from './containers/home'
+import Places from './containers/places';
 
 export const App = () => {
   moment().locale('nn');
