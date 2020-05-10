01c18b77373a438a539c26fbafb89512c5832df6
Tore Stensaker Tefre <tore@tefre.it>
2020-05-10 19:37:51 +0200

    add scooter data

--- src/components/ScooterCard/index.js
+++ src/components/ScooterCard/index.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDistanceFromLatLonInKm } from './../../utils/distance'
 
-export const ScooterCard = () => {
+export const ScooterCard = props => {
+  const { geoLocation } = props;
+  const [scooterData, setScooterData] = useState();
+
+  useEffect(() => {
+    const fetchScooters = async () => {
+      const response = await fetch(
+        `https://api.entur.io/mobility/v1/scooters?lat=${geoLocation.lat}&lon=${geoLocation.lon}&display=10`,
+        {
+          headers: {
+            'ET-Client-Name': 'toretefre - infoscreen'
+          }
+        },
+      )
+      const data = await response.json();
+      setScooterData(data)
+    }
+
+    fetchScooters();
+  }, [geoLocation.lat, geoLocation.lon]);
+
+  if (!scooterData) return <section id="scooter" className="card" />
+
   return (
     <section id="scooter" className="card">
       <table>
         <tbody>
-          <tr>
-            <th>Tier</th>
-            <td>
-              0 sparkesyklar
-            </td>
-            <td>20 meter</td>
-          </tr>
-          <tr>
-            <th>Voi</th>
-            <td>
-              0 sparkesyklar
-            </td>
-            <td>38 meter</td>
-          </tr>
-          <tr>
-            <th>Zvipp</th>
-            <td>
-              0 sparkesyklar
-            </td>
-            <td>80 meter</td>
-          </tr>
+          {scooterData.slice(0, 3).map(scooter =>
+            (<tr>
+              <th>{scooter.operator}</th>
+              <td>
+                {scooter.battery}% batteri
+              </td>
+              <td>
+                {(getDistanceFromLatLonInKm(geoLocation.lat, geoLocation.lon, scooter.lat, scooter.lon) * 1000).toFixed(0)} meter unna
+              </td>
+            </tr>)
+          )}
         </tbody>
       </table>
-      <h6>Sparkesykkeldata i 200 meters omkrets levert av Entur</h6>
+      <h6>Sparkesykkeldata levert av Entur</h6>
     </section>
   );
 }
