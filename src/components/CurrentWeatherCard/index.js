fbaf9d88af039db2d57756e6e227e2ada68c1d0e
Tore Stensaker Tefre <tore.s.tefre@ntnu.no>
2021-04-12 15:49:22 +0200

    lift state from CurrentWeatherCard

--- src/components/CurrentWeatherCard/index.js
+++ src/components/CurrentWeatherCard/index.js
@@ -1,74 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import moment from 'moment';
 import 'moment-timezone';
 import { VictoryArea, VictoryChart, VictoryAxis } from 'victory';
 
 export const CurrentWeatherCard = props => {
-    const { geoLocation } = props;
-    const [weather, setWeather] = useState({
-        loading: true,
-        error: null,
-        lastUpdated: null,
-        precipitation: {
-            data: null,
-            radarCoverage: null,
-        },
-    });
-
-    useEffect(() => {
-        const fetchCurrentWeather = async () => {
-            try {
-                const response = await fetch(
-                    `https://api.met.no/weatherapi/nowcast/2.0/complete?lat=${geoLocation.lat}&lon=${geoLocation.lon}`
-                );
-                const responseJson = await response.json();
-                const fetchedNowcast = responseJson.properties
-
-                const lastUpdatedTime = fetchedNowcast.meta.updated_at;
-                const currentData = fetchedNowcast.timeseries;
-                const precipitationChartData = [];
-                const startTime = fetchedNowcast.timeseries[0].time;
-                const endTime = fetchedNowcast.timeseries[fetchedNowcast.timeseries.length - 1].time;
-                const radarStatus = fetchedNowcast.meta.radar_coverage;
-                const radarCoverage = (radarStatus === "ok") ? true : (radarStatus === "temporarily_unavailable" ? "temporarily_unavailable" : false)
-                let totalPrecipitation = 0
-                
-                currentData
-                    .filter(time =>
-                        moment(time.time).diff(moment(), 'minutes') >= -5
-                    )
-                    .forEach(time => {
-                        const p = parseFloat(time.data.instant.details.precipitation_rate)
-                        totalPrecipitation += p
-                        precipitationChartData.push({
-                            x: moment(time.time).tz('Europe/Oslo').format('LT'),
-                            y: p,
-                        });
-                    });
-
-                setWeather({
-                    loading: false,
-                    error: null,
-                    lastUpdated: lastUpdatedTime,
-                    precipitation: {
-                        radarCoverage: radarCoverage,
-                        data: {
-                            chartData: precipitationChartData,
-                            startTime: startTime,
-                            endTime: endTime,
-                            total: totalPrecipitation,
-                        }
-                    }
-                });
-            }
-            catch {
-                setWeather({
-                    error: "Nedbørsvarsel er diverre ikkje tilgjengeleg nett no 😢"
-                })
-            }
-        };
-        fetchCurrentWeather();
-    }, [geoLocation.lat, geoLocation.lon])
+    const { weather } = props;
 
     if (!weather.precipitation) return <p>Ingen værdata</p>;
     if (weather.loading) return <p>Laster</p>;
@@ -80,8 +16,6 @@ export const CurrentWeatherCard = props => {
         if (millimeters === 3) return "☔"
         return millimeters
     }
-
-    console.log("weather", weather)
 
     if (weather.loading) return (
         <section id="precipitationCard" className="card">
