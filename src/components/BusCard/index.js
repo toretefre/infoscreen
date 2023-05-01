c20277a8de49d0991138429781257e1c00b6e377
Tore Stensaker Tefre <tore@tefre.it>
2023-05-01 21:10:36 +0200

    add distance to buses

--- src/components/BusCard/index.js
+++ src/components/BusCard/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import moment from "moment";
 import "moment-timezone";
+import { getDistance } from "geolib";
 
-export const BusCard = ({ busData, setNumberOfQuays, numberOfQuays }) => {
+export const BusCard = ({ busData, combinedData, setNumberOfQuays, numberOfQuays }) => {
+  console.log(combinedData)
   if (busData.length < 1)
     return (
       <section id="busCard" className="card">
@@ -50,7 +52,20 @@ export const BusCard = ({ busData, setNumberOfQuays, numberOfQuays }) => {
                       "seconds"
                     ) >= 0
                 )
-                .map((departure) => (
+                .map((departure) => {
+                  let vmDataForDeparture = null
+                  let distanceFromQuayToDeparture = null
+
+                  if (combinedData?.data?.length) {
+                    vmDataForDeparture = combinedData?.data?.find(entry => entry.vmData.serviceJourney.id === departure.id)
+                  }
+                  
+                  console.log("fordep", vmDataForDeparture)
+                  if (vmDataForDeparture) {
+                    distanceFromQuayToDeparture = getDistance({lat: quay.lat, lon: quay.lon}, {lat: vmDataForDeparture.vmData.location.latitude, lon: vmDataForDeparture.vmData.location.longitude})
+                  }
+                  vmDataForDeparture && console.log("found vmdata!", vmDataForDeparture)
+                  return (
                   <div className="busContainer" key={departure.id}>
                     <div className="bus">
                       <h3>{departure.line}</h3>
@@ -63,37 +78,46 @@ export const BusCard = ({ busData, setNumberOfQuays, numberOfQuays }) => {
                       {departure.frontText.split(" ")[1] !== "S" && (
                         <h5>{departure.frontText.split(" ")[0]}</h5>
                       )}
+
+                    <div className="depdetails">
+                      {distanceFromQuayToDeparture && <p className="meterText">{`${distanceFromQuayToDeparture > 999 ? (distanceFromQuayToDeparture / 1000).toFixed(1) + " km" : distanceFromQuayToDeparture + " m"}`}</p>}
+                      {!distanceFromQuayToDeparture && <br />}
+                      <p>
+                        {!departure.realtime && "ca "}
+                        {moment(departure.expectedArrivalTime).diff(
+                          moment(),
+                          "seconds"
+                        ) <= 120 &&
+                          moment(departure.expectedArrivalTime).diff(
+                            moment(),
+                            "seconds"
+                          ) + " s"}
+                        {moment(departure.expectedArrivalTime).diff(
+                          moment(),
+                          "seconds"
+                        ) > 120 &&
+                          moment(departure.expectedArrivalTime).diff(
+                            moment(),
+                            "seconds"
+                          ) <
+                            60 * 10 &&
+                          moment(departure.expectedArrivalTime).diff(
+                            moment(),
+                            "minutes"
+                          ) + " min"}
+                        {moment(departure.expectedArrivalTime).diff(
+                          moment(),
+                          "seconds"
+                        ) >=
+                          60 * 10 &&
+                          moment(departure.expectedArrivalTime).format("LTS")}
+                      </p>                      
                     </div>
-                    {!departure.realtime && "ca "}
-                    {moment(departure.expectedArrivalTime).diff(
-                      moment(),
-                      "seconds"
-                    ) <= 120 &&
-                      moment(departure.expectedArrivalTime).diff(
-                        moment(),
-                        "seconds"
-                      ) + " s"}
-                    {moment(departure.expectedArrivalTime).diff(
-                      moment(),
-                      "seconds"
-                    ) > 120 &&
-                      moment(departure.expectedArrivalTime).diff(
-                        moment(),
-                        "seconds"
-                      ) <
-                        60 * 10 &&
-                      moment(departure.expectedArrivalTime).diff(
-                        moment(),
-                        "minutes"
-                      ) + " min"}
-                    {moment(departure.expectedArrivalTime).diff(
-                      moment(),
-                      "seconds"
-                    ) >=
-                      60 * 10 &&
-                      moment(departure.expectedArrivalTime).format("LTS")}
+                    
+                    </div>
+                    
                   </div>
-                ))}
+                )})}
             </section>
           </section>
         ))}
